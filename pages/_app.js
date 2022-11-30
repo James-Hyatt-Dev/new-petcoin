0c5343dc41836bf8a7880061576c98026e404f60
James Hyatt <76410700+James-Hyatt-Dev@users.noreply.github.com>
2022-11-30 17:03:16 -0500

    adds Fragment to the app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { Fragment } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
 
 export default MyApp
